c05e9121a1d2bb703e5421464cdc22667e93125c
Tale <giangnguyen.tale@gmail.com>
2018-10-05 14:06:11 +0700

    fixed warning

--- app/src/main/java/com/besimplify/android/stackoverflowuser/extensions/DateExtensions.kt
+++ app/src/main/java/com/besimplify/android/stackoverflowuser/extensions/DateExtensions.kt
@@ -3,4 +3,4 @@ package com.besimplify.android.stackoverflowuser.extensions
 import java.text.SimpleDateFormat
 import java.util.Date
 
-fun Date.format() = SimpleDateFormat.getDateTimeInstance().format(this)
+fun Date.format(): String = SimpleDateFormat.getDateTimeInstance().format(this)
