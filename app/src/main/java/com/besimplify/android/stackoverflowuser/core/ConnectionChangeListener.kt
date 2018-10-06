f37a4583e1a5e52637db6acb9da75588f84fd07d
Tale <giangnguyen.tale@gmail.com>
2018-10-06 09:32:46 +0700

    refactor -> epose isConnected as Context extension

--- app/src/main/java/com/besimplify/android/stackoverflowuser/core/ConnectionChangeListener.kt
+++ app/src/main/java/com/besimplify/android/stackoverflowuser/core/ConnectionChangeListener.kt
@@ -8,7 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import com.besimplify.android.stackoverflowuser.extensions.isConnected
 
 class ConnectionChangeListener(
   lifecycle: Lifecycle,
@@ -18,10 +18,7 @@ class ConnectionChangeListener(
 
   private val broadcastReceiver = object : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-      val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-      val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-      val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
-      callback.invoke(isConnected)
+      callback.invoke(context?.isConnected() ?: false)
     }
   }
 
