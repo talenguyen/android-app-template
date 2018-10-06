891e62a3326618ce17f1ce299f2185db802ed61c
Tale <giangnguyen.tale@gmail.com>
2018-10-06 13:07:41 +0700

    added missing query

--- app/src/main/java/com/besimplify/android/stackoverflowuser/network/StackOverflowService.kt
+++ app/src/main/java/com/besimplify/android/stackoverflowuser/network/StackOverflowService.kt
@@ -6,6 +6,7 @@ import com.besimplify.android.stackoverflowuser.models.User
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface StackOverflowService {
@@ -21,6 +22,7 @@ interface StackOverflowService {
   @Headers("Accept: application/json;charset=utf-t")
   @GET("users/{user_id}/reputation-history")
   fun reputationHistory(
+    @Path("user_id") userId: Long,
     @Query("site") site: String = "stackoverflow",
     @Query("page") page: Int = 1,
     @Query("pagesize") limit: Int = 30
