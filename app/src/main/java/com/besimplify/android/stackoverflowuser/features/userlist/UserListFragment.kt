72aed0a00b104ed91a59b42c9a93f0eeaac46ed6
Tale <giangnguyen.tale@gmail.com>
2018-10-06 07:41:54 +0700

    show title

--- app/src/main/java/com/besimplify/android/stackoverflowuser/features/userlist/UserListFragment.kt
+++ app/src/main/java/com/besimplify/android/stackoverflowuser/features/userlist/UserListFragment.kt
@@ -1,7 +1,10 @@
 package com.besimplify.android.stackoverflowuser.features.userlist
 
+import android.os.Bundle
+import android.view.View
 import com.airbnb.epoxy.EpoxyController
 import com.airbnb.mvrx.fragmentViewModel
+import com.besimplify.android.stackoverflowuser.R
 import com.besimplify.android.stackoverflowuser.core.ListFragment
 import com.besimplify.android.stackoverflowuser.extensions.format
 import com.besimplify.android.stackoverflowuser.models.User
@@ -11,6 +14,11 @@ import java.util.Date
 class UserListFragment : ListFragment<User>() {
 
   override val viewModel: UserListViewModel by fragmentViewModel()
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    toolbar.title = getString(R.string.app_name)
+  }
 
   override fun EpoxyController.renderItem(item: User) {
     userRow {
